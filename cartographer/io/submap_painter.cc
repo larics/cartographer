921af0325aa91ea874ba4ff5fab295dbf876ef6a
Ivo Vatavuk <vatavuk.ivo@gmail.com>
2019-12-09 13:45:40 +0100

    Make obstacles 100% occupied

--- cartographer/io/submap_painter.cc
+++ cartographer/io/submap_painter.cc
@@ -206,9 +206,11 @@ UniqueCairoSurfacePtr DrawTexture(const std::vector<char>& intensity,
     // We use the red channel to track intensity information. The green
     // channel we use to track if a cell was ever observed.
     const uint8_t intensity_value = intensity.at(i);
-    const uint8_t alpha_value = alpha.at(i);
-    const uint8_t observed =
-        (intensity_value == 0 && alpha_value == 0) ? 0 : 255;
+    uint8_t alpha_value = alpha.at(i);
+    if (alpha_value > 0 && intensity_value == 0) {
+      alpha_value = 255;
+    }
+
     cairo_data->push_back((alpha_value << 24) | (intensity_value << 16) |
                           (observed << 8) | 0);
   }
